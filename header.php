60b6dee8b17cd9147d591e4ae7d0a93554ea207b
nishanmahato <155823436+nishanmahato@users.noreply.github.com>
2024-01-06 19:18:29 +0545

    Add files via upload

---
+++ header.php
@@ -0,0 +1,39 @@
+<nav class="navbar navbar-expand-lg navbar-light fixed-top">
+  <div class="container">
+    <a class="navbar-brand" href="index.php">Fit Gurukul.</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="about-us.php">About Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="news.php">News and Events</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="photo.php">Photo Gallery</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="video.php">Videos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="team.php">Our Team</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contact.php">Contact</a>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+            <a href="book-now.php" class="btn btn-outline-warning">Register Now</a>
+        </form>
+    </div>
+  </div>
+</nav>
+
+
